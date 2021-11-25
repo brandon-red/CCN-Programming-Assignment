e45ec4e73b44356e365e48bd6f2e6db042ce6f96
brandon-red <brandonred627@gmail.com>
2021-11-24 19:37:26 -0500

    Part B functioning
    Part B functioning, need to clean up code and comments and work on README

--- Part B/sftpClient.py
+++ Part B/sftpClient.py
@@ -4,6 +4,16 @@ import sys
 import random
 import os
 import filecmp
+import difflib
+
+def findDiff(file1, file2):
+	f1 = open(file1, 'r')
+	f2 = open(file2, 'r')
+	f1_data = f1.read()
+	f2_data = f2.read()
+	for line in difflib.unified_diff(f1_data, f2_data):
+		print(line)
+
 
 #checking command line for correct input
 if (len(sys.argv) != 2):
@@ -22,19 +32,23 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 #integralMult = ((os.path.getsize("inputfile.txt") % 512)== 0)
 
 #open the input file to be read in binary form
-f = open("inputfile.txt", "r", newline = "\r\n")
-newF = f.read().encode()
+f = open("inputfile.txt", "rb")
 
 fileSend = True
 lastPacket = False
 seqNum = 0
+packetSent = False
 
 #timer started for entire file transfer
 start = time.perf_counter_ns()
 while True:
 
-	data = newF.read(512)
+	#read 479 bytes of data, turns into 512 bytes due to overhead
+	data = f.read(479)
 	print(sys.getsizeof(data))
+	#ensure ASCII encoding 
+	#data = data_raw.encode('ascii')
+	#print(sys.getsizeof(data))
 	#indicate that this is the last packet to be sent
 	if(sys.getsizeof(data) < 512):
 		lastPacket = True
@@ -44,7 +58,7 @@ while True:
 	#creates message with seqNum header and 512 bytes unless it reaches end of file
 	message = seqNumInBytes + data
 	
-	print(message)
+	#print(message)
 
 	#retrainsmission loop for a max of 6 tries
 	for i in range(6):
@@ -72,7 +86,7 @@ while True:
 	#if last packet is successesfully sent, end while loop 
 	if(packetSent and lastPacket):
 		print("done")
-		transferTime = (time.perf_counter_ns() - start)/100000000
+		transferTime = (time.perf_counter_ns() - start)/1000000000
 		f.close()
 		clientSocket.close()
 		break
@@ -88,10 +102,11 @@ while True:
 		break
 
 print(fileSend)
-print(filecmp.cmp("inputfile.txt", "outputfile.txt", shallow = False))
+fileCompare = filecmp.cmp("inputfile.txt", "outputfile.txt", shallow = False)
+print(fileCompare)
 #if the file was sent, and if the files are matching
-if(fileSend and filecmp.cmp("inputfile.txt", "outputfile.txt", shallow = False)):
-	print("sFTP: file sent successfully to {} in {:.3}secs".format(serverIP, transferTime))
+if(fileSend and fileCompare):
+	print("sFTP: file sent successfully to {} in {:.3f}secs".format(serverIP, transferTime))
 
 else:
 	print("sFTP: file transfer unsuccessful: packet retransmission limit reached")
--- Part B/sftpServer.py
+++ Part B/sftpServer.py
@@ -1,6 +1,7 @@
 import random
 import time
 import sys
+import filecmp
 from socket import *
 
 # Create UDP socket
@@ -8,11 +9,11 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Assign IP address and port number to socket
 serverSocket.bind(('', 9093))
 # Assign Loss and Delay parameters
-LOSS_RATE = 0
-AVERAGE_DELAY = 0
+LOSS_RATE = 0.2
+AVERAGE_DELAY = 100
 
-#opens file for writing, must write in text, so decode message from client
-f = open("outputfile.txt", "w", newline="\r\n")
+#opens file for writing, and have newline set to \r\n (for windows)
+f = open("outputfile.txt", "wb")
 
 expect = 0
 
@@ -22,9 +23,9 @@ while True:
 	message, address = serverSocket.recvfrom(513)
 
 	#print(message)
-	print(message[0])
+	#print(message[0])
 
-	print(message[1:])
+	#print(message[1:])
 	# Decide whether to reply, or simulate packet loss. If rand is less than LOSS_RATE, we consider the packet lost and do not respond
 	if rand < LOSS_RATE:
 	   print("Lost packet or ack")
@@ -37,20 +38,22 @@ while True:
 
 	#last packet sent and if it is in correct order, write then close file and exit loop
 	if(sys.getsizeof(message)<513 and (expect == message[0])):
-		f.write(message[1:].decode())
+		f.write(message[1:])
 		serverSocket.sendto(message[0].to_bytes(1, "big"), address)
-		print("done")
+		f.close()
+		#print("done")
 		break
 	#check if sequence number matches what receiver is expecting
 	elif(expect == message[0]):
-		print("written")
-		f.write(message[1:].decode())
+		f.write(message[1:])
+		#print("written")
 		expect = abs(expect-1)
 		#send ack
 		serverSocket.sendto(message[0].to_bytes(1, "big"), address)
 	#receive duplicate therefore send ack, but dont write into file
 	else:
 		serverSocket.sendto(message[0].to_bytes(1, "big"), address)
+
 
 
 
